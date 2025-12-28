ad436d3c0a5beac60ed415cc9ac7773242d0a2db
PierreBou <pierre.bouriat@outlook.fr>
2025-12-28 11:41:12 +0100

    chore: upgrade dependencies and add request logging for dokploy

--- src/main.rs
+++ src/main.rs
@@ -103,7 +103,8 @@ async fn readme() -> Result<HttpResponse> {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::init();
+    // Initialize logger with default level if RUST_LOG is not set
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
 
     let host = env::var("HOST").unwrap_or_else(|_| "0.0.0.0".to_string());
     let port = env::var("PORT")
@@ -115,7 +116,7 @@ async fn main() -> std::io::Result<()> {
 
     HttpServer::new(|| {
         App::new()
-            .wrap(Logger::default())
+            .wrap(Logger::new("%a \"%r\" %s"))
             .service(standard)
             .service(readme)
     })
